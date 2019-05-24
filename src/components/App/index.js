02804fba9856a000560ea1c50f2ea27f11f15530
Indika Piyasena <indika.piyasena@gmail.com>
2019-05-24 20:06:37 +0800

    add some tabs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Block from './../Block';
+import Home from './../Home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        UND Block Explorer
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>UND Block Explorer</h2>
+                </div>
+                <div className="App-nav">
+                    <Router>
+                        <div>
+                            <Link to="/">Home</Link>
+                            <Link to="/block">Block</Link>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/block" render={() => (
+                                <h3>Please select a blockHash.</h3>
+                            )} />
+                            <Route path="/block/:blockHash" component={Block} />
+                        </div>
+                    </Router>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
